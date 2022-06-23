17e235d53b46a3496f90eac94fdf3fc0a7fa6727
Edmur <edmurneves@gmail.com>
2022-06-23 16:47:48 -0300

    Criado o middleware checkUser, e o post de userController.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
+const checkUser = require('../middleware/checkUser');
+
 const userRouter = express.Router();
 const userService = require('../services/userService');
 
 userRouter.get('/', async (req, res) => {    
     const users = await userService.getUsers();   
     res.status(201).json(users);
+});
+
+userRouter.post('/', checkUser, async (req, res) => {
+    const newUser = await userService.createUser(req.body);    
+    res.status(201).json(newUser);
 });
 
 module.exports = userRouter;
