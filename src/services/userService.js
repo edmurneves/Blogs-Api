ac7f2c96110836d9cbd3b7b2cbac68fa51680c8b
Edmur <edmurneves@gmail.com>
2022-06-23 16:49:32 -0300

    Criado o método createUser de userService.

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,5 @@
 const { User } = require('../database/models');
+const { generateJWTToken } = require('../utils/jwt');
 
 const getUsers = async () => {    
     const users = await User.findAll({
@@ -7,6 +8,25 @@ const getUsers = async () => {
     return users;
 };
 
+const createUser = async ({ displayName, email, password, image }) => {
+    const newUser = await User.findOne({
+        attributes: ['displayName', 'email'],
+        where: { email },
+    });
+
+    if (newUser) {
+        const error = { status: 409, message: 'User already registered' };
+        throw error;
+    }
+
+    await User.create({ displayName, email, password, image });
+
+    const token = generateJWTToken({ displayName, email });
+
+    return { token };
+};
+
 module.exports = {
     getUsers,
+    createUser,
 };
