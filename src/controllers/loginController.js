46294f8a0b69fba22b1a2eca1312373f1f49c824
Edmur <edmurneves@gmail.com>
2022-06-23 11:12:49 -0300

    Criado o controller de login.

---
+++ src/controllers/loginController.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const loginService = require('../services/loginService');
+
+const loginRouter = express.Router();
+
+loginRouter.post('/', async (req, res) => {  
+    const token = await loginService.authenticate(req.body);
+    res.status(200).json(token);
+});
+
+module.exports = loginRouter;
