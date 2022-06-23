7b46014bc0d3c9e392e25f44eb800dc0a7cdec4f
Edmur <edmurneves@gmail.com>
2022-06-23 11:12:04 -0300

    Adicionado o middleware de error e a lib express-async-errors.

--- src/api.js
+++ src/api.js
@@ -1,10 +1,15 @@
+require('express-async-errors');
 const express = require('express');
 
-// ...
+const errorMiddleware = require('./middleware/error');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(require('./router'));
+
+app.use(errorMiddleware);
 
 // ...
 
