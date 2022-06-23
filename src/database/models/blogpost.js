75c7904868c836f90f7b13b002b2dc25a1583186
Edmur <edmurneves@gmail.com>
2022-06-23 11:13:34 -0300

    correção da model blogspot

--- src/database/models/blogpost.js
+++ src/database/models/blogpost.js
@@ -1,11 +1,17 @@
 const BlogPostSchema = (sequelize, DataTypes) => {
   const BlogPostTable = sequelize.define('BlogPost', {
-    id: DataTypes.INTEGER,
+    id: { 
+      type: DataTypes.INTEGER, 
+      primaryKey: true, 
+      autoIncrement: true
+    },
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: DataTypes.INTEGER,
     published: DataTypes.DATE,
     updated: DataTypes.DATE
+  }, {
+    timestamps: false,
   });
 
   BlogPostTable.associate = (models) => {
