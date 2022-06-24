75e284da4a930dff3b0421f9d658f9e1096fd953
Edmur <edmurneves@gmail.com>
2022-06-24 10:56:23 -0300

    Criada a rota getById do controller user.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,6 +11,12 @@ userRouter.get('/', authenticateMiddleware, async (req, res) => {
     res.status(200).json(users);
 });
 
+userRouter.get('/:id', authenticateMiddleware, async (req, res) => {
+    const { id } = req.params;
+    const user = await userService.getById(id);
+    res.status(200).json(user);
+});
+
 userRouter.post('/', checkUser, async (req, res) => {
     const newUser = await userService.createUser(req.body);    
     res.status(201).json(newUser);
