7562501fb100f4b51033b320505ec03c839ff160
Edmur <edmurneves@gmail.com>
2022-06-24 10:25:48 -0300

    Criada a função de verificação de token.

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -11,6 +11,22 @@ const jwtConfig = {
 const generateJWTToken = ({ displayName, email }) => 
     jwt.sign({ displayName, email }, JWT_SECRET, jwtConfig);
 
+const authenticateToken = async (token) => {
+    if (!token) {
+        const error = { status: 401, message: 'Token not found' };
+        throw error;
+    }
+
+    try {
+        const validate = await jwt.verify(token, JWT_SECRET);        
+        return validate;
+    } catch (error) {
+        const errorMessage = { status: 401, message: 'Expired or invalid token' };
+        throw errorMessage;
+    }
+};
+
 module.exports = {
     generateJWTToken,
+    authenticateToken,
 };
