369cbd9e46b27696ff51f0620cfb74e547cf64fa
Edmur <edmurneves@gmail.com>
2022-06-24 16:43:46 -0300

    Adicionada a rota /categories

--- src/router.js
+++ src/router.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const loginController = require('./controllers/loginController');
 const userController = require('./controllers/userController');
+const categoriesController = require('./controllers/categoriesController');
 
 const routers = express.Router();
 routers.use('/login', loginController);
 routers.use('/user', userController);
+routers.use('/categories', categoriesController);
 
 module.exports = routers;
