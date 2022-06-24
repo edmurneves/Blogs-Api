f78835689601251e724fb5c64122f97880dac63e
Edmur <edmurneves@gmail.com>
2022-06-24 17:33:30 -0300

    Criado o método getCategories de Service.

--- src/services/categoriesService.js
+++ src/services/categoriesService.js
@@ -10,6 +10,9 @@ const createCategory = async ({ name }) => {
     return newCategory;
 };
 
+const getCategories = async () => Category.findAll();
+
 module.exports = {
     createCategory,
+    getCategories,
 };
