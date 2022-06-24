cc565d17322f726de6c2c55b7204b1c86f59e5a6
Edmur <edmurneves@gmail.com>
2022-06-24 17:33:05 -0300

    Criada a rota get categories

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -5,8 +5,13 @@ const authenticateMiddleware = require('../middleware/auth');
 const categoriesRouter = express.Router();
 
 categoriesRouter.post('/', authenticateMiddleware, async (req, res) => {  
-    const category = await categoriesService.createCategory(req.body);
-    res.status(201).json(category);
+    const newCategory = await categoriesService.createCategory(req.body);
+    res.status(201).json(newCategory);
+});
+
+categoriesRouter.get('/', authenticateMiddleware, async (req, res) => {
+    const categories = await categoriesService.getCategories();
+    res.status(200).json(categories);
 });
 
 module.exports = categoriesRouter;
