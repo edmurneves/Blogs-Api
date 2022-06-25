6c2b545c08b15a4e46a4a4fc2620b0540d0ff164
Edmur <edmurneves@gmail.com>
2022-06-24 21:10:11 -0300

    Adicionada a rota /post.

--- src/router.js
+++ src/router.js
@@ -3,10 +3,12 @@ const express = require('express');
 const loginController = require('./controllers/loginController');
 const userController = require('./controllers/userController');
 const categoriesController = require('./controllers/categoriesController');
+const postController = require('./controllers/postController');
 
 const routers = express.Router();
 routers.use('/login', loginController);
 routers.use('/user', userController);
 routers.use('/categories', categoriesController);
+routers.use('/post', postController);
 
 module.exports = routers;
