0e341d384254fae90927e53fd3fff869fb76bb7e
Edmur <edmurneves@gmail.com>
2022-06-27 16:33:32 -0300

    Criado o controller de post.

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -5,8 +5,15 @@ const authenticateMiddleware = require('../middleware/auth');
 const postRouter = express.Router();
 const postService = require('../services/postService');
 
-postRouter.post('/', async (req, res) => {
-    const newPost = await postService.createPost(req.body);
+postRouter.post('/', authenticateMiddleware, async (req, res) => {
+    const token = req.headers.authorization;
+    const { title, content, categoryIds } = req.body;
+
+    if (!title || !content || !categoryIds) {
+        return res.status(400).json({ message: 'Some required fields are missing' });
+    }   
+  
+    const newPost = await postService.createPost(token, title, content, categoryIds);
     res.status(201).json(newPost);
 });
 
