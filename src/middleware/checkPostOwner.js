27ceb76891757428df47bc670e9717da7a8abd84
Edmur <edmurneves@gmail.com>
2022-06-29 10:30:57 -0300

    Criado o middleware checkPostOwner.

---
+++ src/middleware/checkPostOwner.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const postService = require('../services/postService');
+
+const secretKey = process.env.JWT_SECRET;
+
+const checkPostOwner = async (req, res, next) => {
+    const token = req.headers.authorization;
+    console.log(token);
+    const { id } = jwt.verify(token, secretKey);
+    const post = await postService.getById(req.params.id);
+
+    if (!post) return res.status(404).json({ message: 'Post does not exist' });
+
+    const { dataValues: { userId } } = post;
+
+    if (id !== userId) return res.status(401).json({ message: 'Unauthorized user' });
+
+    next();
+};
+
+module.exports = checkPostOwner;
