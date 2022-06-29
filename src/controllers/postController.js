824e1b1aa03e00c51df10c9bd462835490abe5a5
Edmur <edmurneves@gmail.com>
2022-06-29 10:30:24 -0300

    Criada a rota put de Post.

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const authenticateMiddleware = require('../middleware/auth');
+const checkPostOwner = require('../middleware/checkPostOwner');
 
 const postRouter = express.Router();
 const postService = require('../services/postService');
@@ -26,6 +27,18 @@ postRouter.get('/:id', authenticateMiddleware, async (req, res) => {
     const { id } = req.params;
     const post = await postService.getById(id);
     res.status(200).json(post);
+});
+
+postRouter.put('/:id', authenticateMiddleware, checkPostOwner, async (req, res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    if (!title || !content) {
+        return res.status(400).json({ message: 'Some required fields are missing' });
+    }
+
+    const updatedPost = await postService.updatePost(id, title, content);
+    res.status(200).json(updatedPost);
 });
 
 module.exports = postRouter;
