58f36005f43f48a6803c24831b5d79ffa1534fc8
Edmur <edmurneves@gmail.com>
2022-07-02 21:11:37 -0300

    Criado a rota user/me para remoção do ususário que possui o token válido.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const jwt = require('jsonwebtoken');
+
+const secretKey = process.env.JWT_SECRET;
+
 const checkUser = require('../middleware/checkUser');
 const authenticateMiddleware = require('../middleware/auth');
 
@@ -20,6 +24,15 @@ userRouter.get('/:id', authenticateMiddleware, async (req, res) => {
 userRouter.post('/', checkUser, async (req, res) => {
     const newUser = await userService.createUser(req.body);    
     res.status(201).json(newUser);
+});
+
+userRouter.delete('/me', authenticateMiddleware, async (req, res) => {
+    const token = req.headers.authorization;
+    const { id } = jwt.verify(token, secretKey);    
+
+    await userService.deleteOwner(id);
+
+    res.status(204).end();
 });
 
 module.exports = userRouter;
